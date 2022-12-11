64b0702f555c4d0cb9929027ade614ed796e1b2c
Moses Kisya <kisyamoses12@outlook.com>
2022-12-11 04:28:44 -0800

    print comb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints all possible 3 digits.
+*Return: (0)
+*/
+int main(void)
+{
+	char i;
+	char j;
+	char k;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = i; j < '9'; j++)
+		{
+                                if(i != j)
+                                {
+                                        for (k = j; k <= '9'; k++)
+                                        {
+                                                if(j != k || i !=k)
+                                                {
+                                                        putchar(i);
+                                                        putchar(j);
+                                                        putchar(k);
+                                                
+                                                        if (i != '7' || j != '8' || k != '9')
+                                                        {
+                                                                putchar(',');
+                                                                putchar(' ');
+                                                        }
+                                                }
+                                        }
+                                }
+		}
+	}
+	putchar('\n');
+	return (0);
+}
