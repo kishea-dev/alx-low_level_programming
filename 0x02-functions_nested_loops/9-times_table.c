522f208039d31d18087ee598b03dc3ece25682c0
Moses Kisya <kisyamoses12@outlook.com>
2022-12-13 15:01:29 -0800

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - PRINT 9 MULTIPLES FROM 0 to 9.
+ */
+void times_table(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = num1; num2 <= 9; num2++)
+		{
+			int result = num1 * num2;
+
+			_putchar(',');
+			_putchar(' ');
+			if (result <= 10)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + (result / 10));
+			}
+			_putchar('0' + (result % 10));
+		}
+		_putchar('\n');
+	}
+}
