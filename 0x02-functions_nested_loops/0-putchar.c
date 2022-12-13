b286dd6dbff9b6585bd7cb225e96a2e068610b85
Moses Kisya <kisyamoses12@outlook.com>
2022-12-13 12:09:34 -0800

    README and 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+/**
+* main - prints _putchar
+* Return: 0 - success
+*/
+int main(void)
+{
+	int str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < sizeof(str); i++)
+	{
+		_putchar(ch);
+	}
+	return (0);
+}
