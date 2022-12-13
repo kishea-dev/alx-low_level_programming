ba5dbb28231fd97a2b463267e716a1ab60c7a7bd
Moses Kisya <kisyamoses12@outlook.com>
2022-12-13 12:17:41 -0800

    edited putchat

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,12 +4,12 @@
 */
 int main(void)
 {
-	int str[] = "_putchar";
+	int str[] = {'_','p','u','t','c','h','a','r'};
 	int i;
 
 	for (i = 0; i < sizeof(str); i++)
 	{
-		_putchar(ch);
+		_putchar(i);
 	}
 	return (0);
 }
