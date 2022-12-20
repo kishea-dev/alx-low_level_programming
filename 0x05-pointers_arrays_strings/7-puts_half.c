8494a1e016ed45bfef709606669de1752338bcfb
Moses Kisya <kishea.dev@gmail.com>
2022-12-20 15:42:53 +0300

    puts chlf updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,18 +5,40 @@
  */
 void puts_half(char *str)
 {
-	int i, j;
+	int m, n, i, j;
 
-	while (str[i] != '\0')
-		i++;
-	if (i % 2 == 0)
-		j = i / 2;
-	else
-		j = (i + 1) / 2;
-	while (j < i)
+	m = string_length(str) / 2;
+
+	if (string_length(str) % 2 == 0)
 	{
-		_putchar(str[j]);
-		j++;
+		for (i = m; i < string_length(str); i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (string_length(str) % 2 != 0)
+	{
+		n = (string_length(str) - 1) / 2;
+		for (j = n; j <= string_length(str); j++)
+		{
+			_putchar(str[j]);
+		}
 	}
 	_putchar('\n');
+}
+
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
