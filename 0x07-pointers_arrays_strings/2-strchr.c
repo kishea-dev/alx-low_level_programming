50b43ebea4dbe24bd75620f6dbacaadd36469f1a
Moses Kisya <kishea.dev@gmail.com>
2023-01-04 06:20:20 -0500

    advanced tasks 5th

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,13 +2,6 @@
 #include <stdio.h>
 
 /**
- * *_strchr - locates a character in a string.
- *
- * @s: string.
- * @c: character to be located.
- * Return: pointer to first occurrence of NULL if no character found.
- */
-/**
  * _strchr - locates a character in a string
  * @s: the memory area
  * @c: constant byte
@@ -28,24 +21,4 @@ char *_strchr(char *s, char c)
 		}
 	}
 	return ('\0');
-}
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s = "hello";
-    char *f;
-
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
 }
