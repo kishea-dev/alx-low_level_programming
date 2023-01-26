95c4b456f4b1fe5a569f7f60a4cb2e19a63ece12
moses kisya <kishea.dev@gmail.com>
2023-01-27 01:46:35 +0300

    added 1.asm addded tested 100-main.c pre main execution tested too

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-  * startupfunc - executes before main function is executed.
+  * pre_main - executes before main function is executed.
   * Return: nothing.
   */
-void startupfunc(void)
+void __attribute__ ((constructor)) pre_main()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
