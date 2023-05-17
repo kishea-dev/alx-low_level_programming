a80d8b6f908d94555e2ab5d91d1b7811a52f67bf
Moses Kisya <118076277+kishea-dev@users.noreply.github.com>
2023-05-17 17:15:42 +0300

    print

--- 0x18-dynamic_libraries/win.c
+++ 0x18-dynamic_libraries/win.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int printf()
+int printf (const char *__restrict __format, ...)
 {
         write(1, "9 8 10 24 75 9\n", 14);
         write(1, "Congratulations, you win the Jackpot!\n", 38);
